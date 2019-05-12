8b5f8688232108c9e64ef15162af64cea5151f34
Henrique Melanda <henrique.melanda@C02Y256CJG5H.local>
2019-05-12 20:23:40 -0300

    React Hooks - useEffect: Running only once

---
+++ 07/src/App.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react';
+
+const App = () => (
+  <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+    <CounterClass />
+    <CounterFunction />
+  </div>
+)
+
+class CounterClass extends React.PureComponent {
+  state = {
+    counter: 0,
+    toggle: false,
+  }
+  
+  componentDidMount () {
+    this.updateComponentTitle();
+  }
+
+  componentDidUpdate (prevProps, prevState) {
+    if (prevState.counter !== this.state.counter) {
+      this.updateComponentTitle();
+    }
+  }
+
+  updateComponentTitle() {
+    document.title = `CounterClass: ${this.state.counter}`
+  }
+
+  render () {
+    return (
+      <>
+        <Counter
+          counter={this.state.counter}
+          inclement={() => {
+            this.setState((prevState) => ({
+              counter: prevState.counter + 1
+             }))
+           }}
+            decrement={() => {
+             this.setState((prevState) => ({
+                counter: prevState.counter - 1
+             }))
+           }}
+         />
+         {this.state.toggle && <h1>Toggle</h1>}
+         <button onClick={() => this.setState({
+          toggle: !this.state.toggle
+          })}>Toggle</button>
+      </>
+    )
+  }
+}
+
+function CounterFunction () {
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    console.log('componentDidMount');
+    setInterval(() => {
+      setCounter((counter) => counter + 1);
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    document.title = `CounterFunction: ${counter}`
+  }, [counter]);
+
+  return (
+    <Counter
+      counter={counter}
+      inclement={() => setCounter(counter => counter + 1)}
+      decrement={() => setCounter(counter => counter - 1)}
+    />
+  )
+}
+
+const Counter = ({ counter, inclement, decrement }) => (
+  <div style={{ textAlign:'center' }}>
+    <h1>{ counter }</h1>
+    <button onClick={decrement}>-</button>
+    <button onClick={inclement}>+</button>
+  </div>
+)
+
+export default App;
